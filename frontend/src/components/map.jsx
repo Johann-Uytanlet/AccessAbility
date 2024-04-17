1f4069d151050a14bba391cbad731c12acd0bcc4
Johann-Sven-Lee-Uytanlet <114980320+Johann-Sven-Lee-Uytanlet@users.noreply.github.com>
2024-04-17 09:55:06 +0800

    map page working

--- frontend/src/components/map.jsx
+++ frontend/src/components/map.jsx
@@ -1,33 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+// MapComponent.js
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 
-/*
-import Spinner from '../components/Spinner';
-import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
-import BooksTable from '../components/home/BooksTable';
-import BooksCard from '../components/home/BooksCard';
-*/
-const Map = () => {
-
-  const mapRef = useRef(null);
-  const latitude = 51.505;
-  const longitude = -0.09;
-
+function map() {
   return (
-    <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/* Additional map layers or components can be added here */}
-      </MapContainer>
-   
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '400px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
   );
-};
+}
 
-export default Map;
+export default map;
--- frontend/src/pages/mapPage.jsx
+++ frontend/src/pages/mapPage.jsx
@@ -3,16 +3,11 @@ import axios from 'axios';
 import Map from '../components/map.jsx';
 const mapPage = () => {
 
-  const mapRef = useRef(null);
-  const latitude = 51.505;
-  const longitude = -0.09;
-
   return (
-      <div>
-        asdafsasdasdfsadf
-        
-      </div>
-   
+    <div>
+      <h1>Map Page</h1>
+      <Map />
+    </div>
   );
 };
 
