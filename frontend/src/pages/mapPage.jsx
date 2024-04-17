a6a0211f80694e725ad1c00143ba601ff5d9c8f8
Johann-Sven-Lee-Uytanlet <114980320+Johann-Sven-Lee-Uytanlet@users.noreply.github.com>
2024-04-17 09:42:39 +0800

    frontend stuff

---
+++ frontend/src/pages/mapPage.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Map from '../components/map.jsx';
+const mapPage = () => {
+
+  const mapRef = useRef(null);
+  const latitude = 51.505;
+  const longitude = -0.09;
+
+  return (
+      <div>
+        asdafsasdasdfsadf
+        
+      </div>
+   
+  );
+};
+
+export default mapPage;
