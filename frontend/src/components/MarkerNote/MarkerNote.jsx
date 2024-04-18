a9cf775aa88abc831a7e97bbfc5014d370c6f249
Johann-Sven-Lee-Uytanlet <114980320+Johann-Sven-Lee-Uytanlet@users.noreply.github.com>
2024-04-19 00:06:26 +0800

    date and protoype voting buttons

--- frontend/src/components/MarkerNote/MarkerNote.jsx
+++ frontend/src/components/MarkerNote/MarkerNote.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Comment = ({ username, comment }) => {
+    const [votes, setVotes] = useState(0);
+
     const handleUpvote = () => {
-        // Implement upvote logic here
+        if (votes < 1) {
+            setVotes(votes + 1);
+        }
     };
 
     const handleDownvote = () => {
-        // Implement downvote logic here
+        if (votes > -1) {
+            setVotes(votes - 1);
+        }
     };
 
     
@@ -18,16 +24,17 @@ const Comment = ({ username, comment }) => {
                 <img src='../../../public/sampleProfilePic.png' alt="Profile" style={{ width: '50px', height: '50px', borderRadius: '50%', }} />
             </div>          
             <div style={{ marginRight: '10px', fontWeight: 'bold', paddingRight: '10px'  }}>{username}</div>
-            <div style={{ width: '30px', height: '30px', borderRadius: '50%', backgroundColor: '#36AE26', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{marginRight: '20px', width: '30px', height: '30px', borderRadius: '50%', backgroundColor: '#36AE26', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 5
             </div>
+            <div>4/20/24</div>
             
         </div>
         <div style={{ maxWidth: '400px', wordWrap: 'break-word', marginLeft: '60px', marginBottom: '20px'}}>{comment}</div>
             
         <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', marginLeft: '60px' }}>
             <button onClick={handleUpvote} style={{ marginRight: '5px' }}><img src='../../../public/up.png'/></button>
-            <span>0 votes</span>
+            <span>{votes} votes</span>
             <button onClick={handleDownvote} style={{ marginLeft: '5px' }}><img src='../../../public/down.png'/></button>
         </div>
         </div>
